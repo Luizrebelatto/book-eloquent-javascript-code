67115f1ff63d08830cc20c71683d994847136542
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2026-05-08 18:43:57 -0300

    while

---
+++ chapter 2/whileAndDoLoops.js
@@ -0,0 +1,18 @@
+// wrong 
+function showNumber0To12(){
+    console.log(0)
+    console.log(2)
+    console.log(4)
+    console.log(6)
+    console.log(8)
+    console.log(10)
+    console.log(12)
+}
+
+function showNumbersWhile(){
+    let number = 0;
+    while(number <= 12){
+        console.log(number)
+        number += 2
+    }
+}
