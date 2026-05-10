4c87e1de50e54056bbfe2cb9720df9598f608ee0
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2026-05-09 22:53:33 -0300

    dynamic board

--- chapter 2/chessBoard.js
+++ chapter 2/chessBoard.js
@@ -16,4 +16,18 @@ function chessBoardSizeFixed(){
     }
 }
 
-chessBoardSizeFixed()
+function chessBoardDynamicSize(size){
+    for (let line = 0; line < size; line++) {
+        let chess = "";
+
+        for (let column = 0; column < size; column++) {
+            if ((line + column) % 2 === 0) {
+                chess += " ";
+            } else {
+                chess += "#";
+            }
+        }
+
+        console.log(chess);
+    }
+}
